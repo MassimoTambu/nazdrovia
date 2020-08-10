e78e9c6d856001ae6b226a84dc0b7d6cd96ac843
Massimo Tamburini <massimo.tamburini.work@gmail.com>
2020-08-10 15:39:24 +0200

    Fixed sort and expanded animation

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -8,6 +8,7 @@ import { MatIconModule } from "@angular/material/icon";
 import { MatInputModule } from "@angular/material/input";
 import { MatMenuModule } from "@angular/material/menu";
 import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatSortModule } from "@angular/material/sort";
 import { MatTableModule } from "@angular/material/table";
 
 const MaterialModules = [
@@ -21,6 +22,7 @@ const MaterialModules = [
   MatPaginatorModule,
   MatFormFieldModule,
   MatInputModule,
+  MatSortModule,
 ];
 
 @NgModule({
--- src/app/shared/universal-table/universal-table.component.ts
+++ src/app/shared/universal-table/universal-table.component.ts
@@ -27,10 +27,14 @@ import { Player } from "src/app/models/player";
   styleUrls: ["./universal-table.component.scss"],
   animations: [
     trigger("detailExpand", [
-      state("collapsed", style({ height: "0px", minHeight: "0" })),
+      state("collapsed, void", style({ height: "0px", minHeight: "0" })),
       state("expanded", style({ height: "*" })),
       transition(
         "expanded <=> collapsed",
+        animate("225ms cubic-bezier(0.4, 0.0, 0.2, 1)")
+      ),
+      transition(
+        "expanded <=> void",
         animate("225ms cubic-bezier(0.4, 0.0, 0.2, 1)")
       ),
     ]),
